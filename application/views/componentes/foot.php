8b04388bb448e01d576f7a0831b30634dbd8400b
luxor6232 <eamunooz@gmail.com>
2023-11-05 20:58:59 -0300

    integracion de navbar, y ajuste al controlador

---
+++ application/views/componentes/foot.php
@@ -0,0 +1,9 @@
+<script src="<?= base_url('assets/js/jquery.js') ?>"></script>
+		<script src="<?= base_url('assets/js/bootstrap.js') ?>"></script>
+		<script src="<?= base_url('assets/bootstrap-table/dist/bootstrap-table.js') ?>"></script>
+		<script src="<?= base_url('assets/alertifyjs/alertify.min.js') ?>"></script>
+		<script src="<?= base_url('assets/bootstrap-duallistbox/dist/jquery.bootstrap-duallistbox.js') ?>"></script>
+
+</body>
+
+</html>
